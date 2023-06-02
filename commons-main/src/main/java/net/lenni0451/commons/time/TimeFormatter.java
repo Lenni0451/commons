6cda43229537cd4819da26893d5b68b553a559f0
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-06-02 18:18:58 +0200

    Added javadoc

--- commons-main/src/main/java/net/lenni0451/commons/time/TimeFormatter.java
+++ commons-main/src/main/java/net/lenni0451/commons/time/TimeFormatter.java
@@ -5,20 +5,51 @@ import java.util.TimeZone;
 
 public class TimeFormatter {
 
+    /**
+     * Format a time in milliseconds to a string.<br>
+     * See {@link SimpleDateFormat} for the format string.
+     *
+     * @param format The format string
+     * @param time   The time in milliseconds
+     * @return The formatted time
+     */
     public static String format(final String format, final long time) {
         return format(format, time, false);
     }
 
+    /**
+     * Format a time in milliseconds to a (UTC) string.<br>
+     * See {@link SimpleDateFormat} for the format string.
+     *
+     * @param format The format string
+     * @param time   The time in milliseconds
+     * @param utc    If the time should be formatted in UTC
+     * @return The formatted time
+     */
     public static String format(final String format, final long time, final boolean utc) {
         SimpleDateFormat sdf = new SimpleDateFormat(format);
         if (utc) sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
         return sdf.format(time);
     }
 
+    /**
+     * Format a time in milliseconds to a string in the format "mm:ss".<br>
+     * e.g. {@code 123000 -> 02:03}
+     *
+     * @param time The time in milliseconds
+     * @return The formatted time
+     */
     public static String formatMMSS(final long time) {
         return format("mm:ss", time, true);
     }
 
+    /**
+     * Format a time in milliseconds to a string in the format "HH:mm:ss".<br>
+     * e.g. {@code 3723000 -> 01:02:03}
+     *
+     * @param time The time in milliseconds
+     * @return The formatted time
+     */
     public static String formatHHMMSS(final long time) {
         return format("HH:mm:ss", time, true);
     }
