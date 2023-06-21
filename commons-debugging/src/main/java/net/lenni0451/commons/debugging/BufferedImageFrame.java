0baf1c9f5d37ac08716f258031112032f404ed65
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-06-21 20:46:36 +0200

    Added BufferedImageFrame

---
+++ commons-debugging/src/main/java/net/lenni0451/commons/debugging/BufferedImageFrame.java
@@ -0,0 +1,48 @@
+package net.lenni0451.commons.debugging;
+
+import javax.annotation.Nullable;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * A simple JFrame that displays a BufferedImage.
+ */
+public class BufferedImageFrame extends JFrame {
+
+    private BufferedImage image;
+
+    public BufferedImageFrame(@Nullable final BufferedImage image) {
+        this.image = image;
+
+        this.setTitle("BufferedImage Frame");
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.initPanes();
+        this.setVisible(true);
+    }
+
+    private void initPanes() {
+        JPanel panel = new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+                if (image == null) {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(0, 0, this.getWidth(), this.getHeight());
+                    g.setColor(Color.BLACK);
+                    g.drawString("No Image", 0, 10);
+                } else {
+                    g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+                }
+            }
+        };
+        this.add(panel);
+    }
+
+    public void setImage(@Nullable final BufferedImage image) {
+        this.image = image;
+        this.repaint();
+    }
+
+}
