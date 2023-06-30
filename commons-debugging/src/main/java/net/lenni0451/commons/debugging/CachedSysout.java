f4f956b786002b238a3b43cad2fd06c9ec72bf74
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-06-30 19:59:16 +0200

    Added CachedSysout

---
+++ commons-debugging/src/main/java/net/lenni0451/commons/debugging/CachedSysout.java
@@ -0,0 +1,52 @@
+package net.lenni0451.commons.debugging;
+
+/**
+ * Print debug messages to the console without spamming the same message multiple times.
+ */
+public class CachedSysout {
+
+    private static String lastMessage;
+
+    public static void println(final boolean b) {
+        println(String.valueOf(b));
+    }
+
+    public static void println(final byte b) {
+        println(String.valueOf(b));
+    }
+
+    public static void println(final char c) {
+        println(String.valueOf(c));
+    }
+
+    public static void println(final short s) {
+        println(String.valueOf(s));
+    }
+
+    public static void println(final int i) {
+        println(String.valueOf(i));
+    }
+
+    public static void println(final long l) {
+        println(String.valueOf(l));
+    }
+
+    public static void println(final float f) {
+        println(String.valueOf(f));
+    }
+
+    public static void println(final double d) {
+        println(String.valueOf(d));
+    }
+
+    public static void println(final Object o) {
+        println(String.valueOf(o));
+    }
+
+    public static void println(final String message) {
+        if (message.equals(lastMessage)) return;
+        lastMessage = message;
+        System.out.println(message);
+    }
+
+}
