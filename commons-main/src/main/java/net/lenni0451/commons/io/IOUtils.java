13e88d6c39c997576c4b0d8433feb702b9399e43
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-07-16 12:43:04 +0200

    Added IOUtils#readResource

--- commons-main/src/main/java/net/lenni0451/commons/io/IOUtils.java
+++ commons-main/src/main/java/net/lenni0451/commons/io/IOUtils.java
@@ -4,8 +4,53 @@ import javax.annotation.WillNotClose;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 
 public class IOUtils {
+
+    /**
+     * Read a resource from the classpath as bytes.<br>
+     * The resource will be searched in the classpath of this class.
+     *
+     * @param path The path to the resource
+     * @return The bytes of the resource or empty if the resource does not exist
+     * @throws IOException If an I/O error occurs
+     */
+    public static Optional<byte[]> readResource(final String path) throws IOException {
+        return readResource(IOUtils.class, path);
+    }
+
+    /**
+     * Read a resource from the classpath as bytes.<br>
+     * The resource will be searched in the classpath of the given class.
+     *
+     * @param clazz The class to search the resource in
+     * @param path  The path to the resource
+     * @return The bytes of the resource or empty if the resource does not exist
+     * @throws IOException If an I/O error occurs
+     */
+    public static Optional<byte[]> readResource(final Class<?> clazz, final String path) throws IOException {
+        try (InputStream is = clazz.getResourceAsStream(path)) {
+            if (is == null) return Optional.empty();
+            return Optional.of(readAll(is));
+        }
+    }
+
+    /**
+     * Read a resource from the classpath as bytes.<br>
+     * The resource will be searched in the classpath of the given class loader.
+     *
+     * @param loader The class loader to search the resource in
+     * @param path   The path to the resource
+     * @return The bytes of the resource or empty if the resource does not exist
+     * @throws IOException If an I/O error occurs
+     */
+    public static Optional<byte[]> readResource(final ClassLoader loader, final String path) throws IOException {
+        try (InputStream is = loader.getResourceAsStream(path)) {
+            if (is == null) return Optional.empty();
+            return Optional.of(readAll(is));
+        }
+    }
 
     /**
      * Read all bytes from a stream.<br>
--- commons-main/src/test/java/net/lenni0451/commons/io/IOUtilsTest.java
+++ commons-main/src/test/java/net/lenni0451/commons/io/IOUtilsTest.java
@@ -4,10 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class IOUtilsTest {
+
+    @Test
+    void readResource() throws IOException {
+        String className = IOUtils.class.getName().replace('.', '/') + ".class";
+        Optional<byte[]> classBytes = IOUtils.readResource("/" + className);
+        Optional<byte[]> classLoaderBytes = IOUtils.readResource(IOUtils.class.getClassLoader(), className);
+        Optional<byte[]> unknownBytes = IOUtils.readResource("unknown");
+
+        assertTrue(classBytes.isPresent());
+        assertTrue(classLoaderBytes.isPresent());
+        assertFalse(unknownBytes.isPresent());
+    }
 
     @Test
     void readAll() throws IOException {
