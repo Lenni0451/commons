88143395730faa0941e980e3fe156e57125f3e64
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-11-16 20:11:31 +0100

    Added more tests

--- commons-main/src/test/java/net/lenni0451/commons/RandomUtilsTest.java
+++ commons-main/src/test/java/net/lenni0451/commons/RandomUtilsTest.java
@@ -3,10 +3,50 @@ package net.lenni0451.commons;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RandomUtilsTest {
+
+    @RepeatedTest(10)
+    void randomStringLength() {
+        int length = RandomUtils.randomInt(1, 100);
+        String randomString = RandomUtils.randomString(length);
+        assertEquals(length, randomString.length());
+    }
+
+    @RepeatedTest(10)
+    void randomStringLengthAndCharset() {
+        int length = RandomUtils.randomInt(1, 100);
+        String charset = "abc";
+        String randomString = RandomUtils.randomString(length, charset);
+        assertEquals(length, randomString.length());
+        assertTrue(randomString.chars().allMatch(c -> charset.indexOf(c) != -1));
+    }
+
+    @RepeatedTest(10)
+    void randomizeCaseLength() {
+        String s = RandomUtils.randomString(10);
+        String randomized = RandomUtils.randomizeCase(s);
+        assertEquals(s.length(), randomized.length());
+    }
+
+    @RepeatedTest(10)
+    void randomElementArray() {
+        Integer[] array = {1, 2, 3, 4, 5};
+        Integer randomElement = RandomUtils.randomElement(array);
+        assertTrue(Arrays.asList(array).contains(randomElement));
+    }
+
+    @RepeatedTest(10)
+    void randomElementList() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Integer randomElement = RandomUtils.randomElement(list);
+        assertTrue(list.contains(randomElement));
+    }
 
     @RepeatedTest(10)
     void randomInt() {
