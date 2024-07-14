b4159b086f860b72825bab2e38db07a43dff97c4
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-07-14 20:19:13 +0200

    Added consumer template

--- buildSrc/src/main/java/net/lenni0451/commons/buildsrc/model/TemplateConfig.java
+++ buildSrc/src/main/java/net/lenni0451/commons/buildsrc/model/TemplateConfig.java
@@ -1,11 +1,13 @@
 package net.lenni0451.commons.buildsrc.model;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class TemplateConfig {
 
     public String template;
     public String target;
+    public JsonObject globals;
     public JsonElement[] variants;
     public JsonElement variables;
 
