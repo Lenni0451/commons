d73c30758ef00e2cb930c0ccb07e2a576ef7f129
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-01-29 18:19:30 +0100

    Fixed VerticalFileChooser no longer being vertical after changing the view

--- commons-swing/src/main/java/net/lenni0451/commons/swing/components/VerticalFileChooser.java
+++ commons-swing/src/main/java/net/lenni0451/commons/swing/components/VerticalFileChooser.java
@@ -3,6 +3,7 @@ package net.lenni0451.commons.swing.components;
 import net.lenni0451.commons.swing.utils.ComponentUtils;
 
 import javax.swing.*;
+import java.beans.PropertyChangeEvent;
 
 /**
  * A JFileChooser that displays the files in a vertical list instead of a horizontal list.
@@ -14,8 +15,18 @@ public class VerticalFileChooser extends JFileChooser {
             if (component instanceof JList<?>) {
                 JList<?> list = (JList<?>) component;
                 list.setLayoutOrientation(JList.VERTICAL);
+                list.addPropertyChangeListener(this::changeOrientation);
             }
         });
+    }
+
+    private void changeOrientation(final PropertyChangeEvent event) {
+        if ("layoutOrientation".equals(event.getPropertyName())) {
+            JList<?> list = (JList<?>) event.getSource();
+            if (list.getLayoutOrientation() != JList.VERTICAL) {
+                list.setLayoutOrientation(JList.VERTICAL);
+            }
+        }
     }
 
 }
