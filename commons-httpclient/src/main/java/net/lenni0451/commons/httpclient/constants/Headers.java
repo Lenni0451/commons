c547056501853be996b4fa2536abe8343dd8a3fc
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-08-09 19:04:11 +0200

    Deleted deprecated classes

--- commons-httpclient/src/main/java/net/lenni0451/commons/httpclient/constants/Headers.java
+++
@@ -1,8 +0,0 @@
-package net.lenni0451.commons.httpclient.constants;
-
-import org.jetbrains.annotations.ApiStatus;
-
-@Deprecated
-@ApiStatus.ScheduledForRemoval //02.06.2025
-public class Headers extends HttpHeaders {
-}
