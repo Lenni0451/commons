d26f362a76c868098d1a785de4934f93fe20eba6
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-02-22 19:12:31 +0100

    Added methods to read/write to/from streams

--- commons-asm/src/main/java/net/lenni0451/commons/asm/io/JarIO.java
+++ commons-asm/src/main/java/net/lenni0451/commons/asm/io/JarIO.java
@@ -2,16 +2,15 @@ package net.lenni0451.commons.asm.io;
 
 import lombok.experimental.UtilityClass;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.annotation.WillClose;
+import java.io.*;
 import java.nio.file.Files;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 @UtilityClass
@@ -33,21 +32,31 @@ public class JarIO {
                 ZipEntry entry = enumeration.nextElement();
                 if (entry.isDirectory()) continue;
                 try (InputStream is = zipFile.getInputStream(entry)) {
-                    entries.put(entry.getName(), read(is));
+                    entries.put(entry.getName(), readBytes(is));
                 }
             }
         }
         return entries;
     }
 
-    private static byte[] read(final InputStream is) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        byte[] buffer = new byte[8192];
-        int read;
-        while ((read = is.read(buffer)) >= 0) {
-            baos.write(buffer, 0, read);
+    /**
+     * Read a jar file from an input stream and return a map of the entries.<br>
+     * Directories will be ignored.
+     *
+     * @param is The input stream to read from. Will be closed after reading.
+     * @return The entries
+     * @throws IOException If an I/O error occurs
+     */
+    public static Map<String, byte[]> read(@WillClose final InputStream is) throws IOException {
+        Map<String, byte[]> entries = new HashMap<>();
+        try (ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.isDirectory()) continue;
+                entries.put(entry.getName(), readBytes(zis));
+            }
         }
-        return baos.toByteArray();
+        return entries;
     }
 
     /**
@@ -59,13 +68,35 @@ public class JarIO {
      * @throws IOException If an I/O error occurs
      */
     public static void write(final File file, final Map<String, byte[]> entries) throws IOException {
-        try (ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(file.toPath()))) {
+        write(Files.newOutputStream(file.toPath()), entries);
+    }
+
+    /**
+     * Write a jar file to a stream from a map of entries.<br>
+     * The map can not contain directories.
+     *
+     * @param os      The output stream to write to. Will be closed after writing.
+     * @param entries The entries to write
+     * @throws IOException If an I/O error occurs
+     */
+    public static void write(@WillClose final OutputStream os, final Map<String, byte[]> entries) throws IOException {
+        try (ZipOutputStream zos = new ZipOutputStream(os)) {
             for (Map.Entry<String, byte[]> entry : entries.entrySet()) {
                 zos.putNextEntry(new ZipEntry(entry.getKey()));
                 zos.write(entry.getValue());
                 zos.closeEntry();
             }
         }
+    }
+
+    private static byte[] readBytes(final InputStream is) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[8192];
+        int read;
+        while ((read = is.read(buffer)) >= 0) {
+            baos.write(buffer, 0, read);
+        }
+        return baos.toByteArray();
     }
 
 }
